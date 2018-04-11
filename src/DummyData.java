9792efdf900a9bc94b3b976644f98f0f85c51149
Pabricka <the.pate@hotmail.com>
2018-04-11 14:05:53 +0300

    Dummydata

--- src/DummyData.java
+++ src/DummyData.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import Models.User;
+
+import java.util.ArrayList;
 
 public class DummyData {
+
+    ArrayList<User> users = new ArrayList<>();
+
+
+    public void createNewUser(String name, String username, String password, Boolean admin){
+        users.add(new User(name,username,password,admin));
+    }
+
+    public void initializeDummyData(){
+
+        createNewUser("Juha Mieto", "Mämmi666", "1234", false);
+        createNewUser("Jari Mentula", "Bull", "maitorahka", true);
+        createNewUser("Alvin J", "Ellis", "dolor", false);
+        createNewUser("Beverly Z", "Lucas", "Nullam", false);
+        createNewUser("Odysseus I", "Russo", "bibendum", false);
+        createNewUser("Carlos U", "Meyers", "Aliquam", false);
+        createNewUser("Kevin M", "Monroe", "ante", false);
+        createNewUser("Clare W", "Hammond", "Quisque", false);
+        createNewUser("Sonya A", "Hopper", "mollis", false);
+
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
 }
