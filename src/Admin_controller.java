1866abdb4a1a4769d2ee28f886b6516b1c10d37f
estula <essi.lahti97@hotmail.com>
2018-04-11 14:29:18 +0300

    ADMIN SCREEN

---
+++ src/Admin_controller.java
@@ -0,0 +1,10 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+
+
+public class Admin_controller {
+
+   @FXML
+    ListView user_list;
+}
